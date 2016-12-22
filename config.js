5ad3d059dc54db6e85e3457c0a1585479a2b3deb
Mazurkiewicz Aliocha <contact@aliocha.be>
2016-12-22 12:17:05 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+CKEDITOR.plugins.addExternal('tooltip', '/tooltip/', 'plugin.js' );
+CKEDITOR.editorConfig = function( config ) {
+	config.extraPlugins = "tooltip";
+	config.toolbar = [
+		{ name: 'document', items: [ 'Source'] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+		{ name: 'editing', items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+		'/',
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'CopyFormatting', 'RemoveFormat' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+		{ name: 'links', items: [ 'Tooltip',  'RemoveTooltip', 'Link', 'Unlink', 'Anchor' ] },
+		{ name: 'insert', items: [ 'Table', 'HorizontalRule', 'SpecialChar' ] },
+		'/',
+		{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+		{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+		{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+		{ name: 'about', items: [ 'About' ] }
+	];
+};
