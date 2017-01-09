a33a8dcdcd66d39eb8cb944dc40d11e2107c5d3c
Mazurkiewicz Aliocha <contact@aliocha.be>
2017-01-09 14:27:03 +0100

    Update plugin.js

--- tooltip/plugin.js
+++ tooltip/plugin.js
@@ -114,30 +114,21 @@ CKEDITOR.removeTooltipCommand.prototype = {
 CKEDITOR.config.tooltip_tag = 'span';
 CKEDITOR.config.tooltip_class = 'source-info';
 CKEDITOR.config.tooltip_html = true;
-CKEDITOR.config.tooltip_toolbar = [
-  {
-    name: 'document',
-    items: [
-      'Source',
-      '-',
-      'NewPage',
-      'Preview',
-      '-',
-      'Templates'
-    ]
-  },
-  [
-    'Cut',
-    'Copy',
-    'Paste',
-    'PasteText',
-    'PasteFromWord',
-    '-',
-    'Undo',
-    'Redo'
-  ],
-  '/', {
-    name: 'basicstyles',
-    items: ['Bold', 'Italic']
-  }
+CKEDITOR.tooltip_toolbar = [
+	{ name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+	{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+	{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+	{ name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+	'/',
+	{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+	{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+	{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+	{ name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+	'/',
+	{ name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+	{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+	{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+	{ name: 'others', items: [ '-' ] },
+	{ name: 'about', items: [ 'About' ] }
 ];
+CKEDITOR.config.tooltip_styleSet = 'default';
